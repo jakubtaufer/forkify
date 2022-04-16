9def382a9f4be845807e24a95fdeb9d894bacf42
jakubtaufer <jakubtaufer95@gmail.com>
2022-04-16 17:28:39 +0200

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '9a20ca05-f5d8-4ede-bcd9-7b925ada77ca';
+export const MODAL_CLOSE_SEC = 2.5;
