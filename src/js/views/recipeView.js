249bde0855a10d497fefe536fe7bb47175df0e37
jakubtaufer <jakubtaufer95@gmail.com>
2022-04-17 13:47:11 +0200

    Removed fractional

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -121,22 +121,22 @@ class RecipeView extends View {
     `;
   }
 
-  _generateMarkupIngredient(ing) {
-    return `
-    <li class="recipe__ingredient">
-      <svg class="recipe__icon">
-        <use href="${icons}#icon-check"></use>
-      </svg>
-      <div class="recipe__quantity">${
-        ing.quantity ? new Fraction(ing.quantity).toString() : ''
-      }</div>
-      <div class="recipe__description">
-        <span class="recipe__unit">${ing.unit}</span>
-        ${ing.description}
-      </div>
-    </li>
-  `;
-  }
+  // _generateMarkupIngredient(ing) {
+  //   return `
+  //   <li class="recipe__ingredient">
+  //     <svg class="recipe__icon">
+  //       <use href="${icons}#icon-check"></use>
+  //     </svg>
+  //     <div class="recipe__quantity">${
+  //       ing.quantity ? new Fraction(ing.quantity).toString() : ''
+  //     }</div>
+  //     <div class="recipe__description">
+  //       <span class="recipe__unit">${ing.unit}</span>
+  //       ${ing.description}
+  //     </div>
+  //   </li>
+  // `;
+  // }
 }
 
 export default new RecipeView();
